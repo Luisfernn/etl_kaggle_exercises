cfafe27522a9599a754d7fab31a675e73cd7871f
Luis Fernando <luisfernandowubi@gmail.com>
2025-10-29 18:42:31 -0300

    Corrigindo indentação

--- cafe_sales/extract.py
+++ cafe_sales/extract.py
@@ -3,22 +3,22 @@ import pandas as pd
 
 def extract_data(file_path: Path = None):
 
-     if file_path is None:
+    if file_path is None:
         base_dir = Path(__file__).resolve().parent
         file_path = base_dir / "data" / "input" / "dirty_cafe_sales.csv"
 
-     df = pd.read_csv(file_path)     
+    df = pd.read_csv(file_path)     
 
-     print(f"\n✅ Arquivo carregado com sucesso: {file_path}")
-     print(f"📊 Linhas: {len(df)}, Colunas: {len(df.columns)}\n")
+    print(f"\n✅ Arquivo carregado com sucesso: {file_path}")
+    print(f"📊 Linhas: {len(df)}, Colunas: {len(df.columns)}\n")
 
-     print("Prévia dos dados (10 primeiras linhas):")
-     print(df.head(10), "\n")
+    print("Prévia dos dados (10 primeiras linhas):")
+    print(df.head(10), "\n")
 
-     print("Prévia das últimas 10 linhas:")
-     print(df.tail(10), "\n")
+    print("Prévia das últimas 10 linhas:")
+    print(df.tail(10), "\n")
 
-     return df
+    return df
 
 if __name__ == "__main__":
     extract_data()
