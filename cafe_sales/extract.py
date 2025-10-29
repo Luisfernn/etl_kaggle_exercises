9cb5815ce74f7ad7fa5677e65081dab483a6c13f
Luis Fernando <luisfernandowubi@gmail.com>
2025-10-29 18:28:20 -0300

    Criando o script de extract

--- cafe_sales/extract.py
+++ cafe_sales/extract.py
@@ -1,4 +1,24 @@
-import pandas as pd
 from pathlib import Path
+import pandas as pd
 
-file_path = 'C:\Users\luluf\OneDrive\Documents\etl_pipeline_kaggle\cafe_sales\data\input'
+def extract_data(file_path: Path = None):
+
+    if file_path is None:
+        base_dir = Path(__file__).resolve().parent
+        file_path = / "data" / "input" / "dirty_cafe_sales.csv"
+
+     df = pd.read_csv(file_path)     
+
+     print(f"\n✅ Arquivo carregado com sucesso: {file_path}")
+     print(f"📊 Linhas: {len(df)}, Colunas: {len(df.columns)}\n")
+
+     print("Prévia dos dados (10 primeiras linhas):")
+     print(df.head(10), "\n")
+
+     print("Prévia das últimas 10 linhas:")
+     print(df.tail(10), "\n")
+
+     return df
+
+if __name__ == "__main__":
+    extract_data()
