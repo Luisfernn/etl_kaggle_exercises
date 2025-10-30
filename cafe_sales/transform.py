9f08f1ec0a3a15dcac29b639c6bc60308edfa94f
Luis Fernando <luisfernandowubi@gmail.com>
2025-10-30 11:32:59 -0300

    Criando o script de transform

--- cafe_sales/transform.py
+++ cafe_sales/transform.py
@@ -3,3 +3,7 @@ import pandas as pd
 def transform_data():
 
     print("🔧 Transformando data...")
+
+    df = df.drop_duplicates()
+
+    df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
