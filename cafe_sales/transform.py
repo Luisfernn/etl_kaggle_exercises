e39a02589c4bdf4ddd16bdcc994a8b933eee74a9
Luis Fernando <luisfernandowubi@gmail.com>
2025-11-03 08:20:45 -0300

    Deletando transform.py para implementar melhorias de transformação

--- cafe_sales/transform.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-
-def transform_data():
-
-    print("🔧 Transformando data...")
-
-    df = df.drop_duplicates()
-
-    df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
