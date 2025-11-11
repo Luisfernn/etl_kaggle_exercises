6f775aae1698d9a5bfad6127a0810f9e09454cfb
Luis Fernando <luisfernandowubi@gmail.com>
2025-11-11 08:42:26 -0300

    Criando o script de pipeline

---
+++ cafe_sales/transform/pipeline.py
@@ -0,0 +1,53 @@
+import logging 
+from pathlib import Path
+import pandas as pd
+
+from cafe_sales.extract import  extract_data
+from cafe_sales.clean_text import clean_text
+from cafe_sales.validate import validate_data
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "data"
+OUTPUT_DIR = BASE_DIR / "output"
+OUTPUT_DIR.mkdir(exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    handlers=[
+        logging.FileHandler(OUTPUT_DIR / "etl.log", mode="a", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
+
+
+def run-pipeline():
+    try:
+        logger.info("🚀 Iniciando pipeline ETL...")
+
+        df = extract_data(DATA_DIR / "dirty_cafe_sales.csv")
+        logger.info(f"📦 Dados extraídos: {len(df)} linhas.")
+
+        df = clean_text(df)
+        logger.info("🧹 Limpeza de texto concluída.")
+
+        df = clean_numeric(df)
+        logger.info("📊 Limpeza numérica concluída.")
+
+        is_valid = validate_data(df)
+        if not is_valid:
+            logger.waring("⚠️ Dados inválidos detectados. Salvamento cancelado.")
+            return
+
+
+        output_file = OUTPUT_DIR / "clean_sales.csv"
+        df.to_csv(output_file, index=False)
+        logger.info(f"✅ Dados salvos com sucesso em: {output_file}")
+
+
+    except Exeption as e:
+        logger.exeption(f"❌ Erro inesperado na execução do pipeline {e}")  
+
+if __name__ == "__main__:
+    run_pipeline()
