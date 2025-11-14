54015802035d5e3642565d186a294399a8f97aa0
Luis Fernando <luisfernandowubi@gmail.com>
2025-11-14 17:04:32 -0300

    Adicionando o caminho do aqruivo atual e do arquivo de output.

--- cafe_sales/main.py
+++ cafe_sales/main.py
@@ -2,5 +2,8 @@ from pathlib import Path
 from cafe_sales.pipeline import run_pipeline
 from cafe_sales.load import load_data
 
+BASE_DIR = Path(__file__).resolve().parent
+OUTPUT_DIR = BASE_DIR / "data" / "output"
+
 if __name__ == "__main__":
     run_pipeline()
