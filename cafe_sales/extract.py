31cfdaca795edee4ede33f87801a7ebc155b7dd6
Luis Fernando <luisfernandowubi@gmail.com>
2025-11-15 09:06:30 -0300

    Acrescentando configs de logging no if para tentar fazer parar a propagação indevida de logging.

--- cafe_sales/extract.py
+++ cafe_sales/extract.py
@@ -35,4 +35,9 @@ def extract_data(file_path: Path = None):
 
 if __name__ == "__main__":
 
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(message)s"))
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    
     extract_data()
