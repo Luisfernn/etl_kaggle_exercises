65100b62781d36e203aeb28d344da63672b2a607
Luis Fernando <luisfernandowubi@gmail.com>
2025-11-15 08:38:16 -0300

    Logging realocado para main.

--- cafe_sales/main.py
+++ cafe_sales/main.py
@@ -5,6 +5,15 @@ from cafe_sales.load import load_data
 BASE_DIR = Path(__file__).resolve().parent
 OUTPUT_DIR = BASE_DIR / "data" / "output"
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    handlers=[
+        logging.FileHandler(OUTPUT_DIR / "etl.log", mode="a", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+
 if __name__ == "__main__":
     df = run_pipeline()
 
