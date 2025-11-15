4f948cea59e92c8d17a0c30aa58d2856655725dd
Luis Fernando <luisfernandowubi@gmail.com>
2025-11-15 08:55:48 -0300

    Corrigindo erros de digitação nos loggings warning.

--- cafe_sales/validate.py
+++ cafe_sales/validate.py
@@ -16,11 +16,11 @@ def validate_data(df: pd.DataFrame) -> bool:
     numeric_cols = ["total_spent", "price_per_unit" "quantity"] 
     for col in numeric_cols:
         if df[col].isna().any():
-            logger.waring(f"⚠️ Valores ausentes em {col}")
+            logger.warning(f"⚠️ Valores ausentes em {col}")
 
     inconsistents = (df["expected_total"] - (df["price_per_unit"] * df["quantity"])).abs() > 0.01
     if inconsistents.any():
-        logger.waring(f"⚠️ {inconsistents.sum()} inconsistências detectadas entre o esperado e o calculado.")
+        logger.warning(f"⚠️ {inconsistents.sum()} inconsistências detectadas entre o esperado e o calculado.")
 
     logger.info("✅ Validação concluída com sucesso.")    
     return df
