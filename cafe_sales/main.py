8b29dead9b661c9815095d28556e2d0d119fdfcf
Luis Fernando <luisfernandowubi@gmail.com>
2025-11-17 10:08:05 -0300

    Alterando o caminho do salvamento dos loggings para o novo diretório.

--- cafe_sales/main.py
+++ cafe_sales/main.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
     handlers=[
-        logging.FileHandler(OUTPUT_DIR / "etl.log", mode="a", encoding="utf-8"),
+        logging.FileHandler(LOG_DIR / "etl.log", mode="a", encoding="utf-8"),
         logging.StreamHandler()
     ]
 )
