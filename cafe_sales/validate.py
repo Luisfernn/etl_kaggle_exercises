d01d04513d3455f91e5e6751bf09ee34ccf9b370
Luis Fernando <luisfernandowubi@gmail.com>
2025-11-19 09:16:14 -0300

    Reformulando completamente o scipt de validate.

--- cafe_sales/validate.py
+++ cafe_sales/validate.py
@@ -4,24 +4,3 @@ import logging
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
-def validate_data(df: pd.DataFrame) -> bool:
-
-    logger.info("🔍 Iniciando validação de integridade dos dados...")
-
-    missing_columns = []
-
-    expected_cols = ["expected_total", "diff", "suspect_transaction"]
-    for col in expected_cols:
-        if col not in df.columns:
-            logger.warning(f"⚠️ Coluna ausente: {col}")
-            missing_columns.append(col)
-        elif df[col].isna().any():
-            logger.warning(f"⚠️ Valores ausentes em: {col}")
-            missing_columns.append(col)
-
-    if len(missing_columns) == 0:
-        logger.info("✅ Validação concluída com sucesso.")
-        return True
-    else:
-        logger.warning("⚠️ Validação encontrou problemas.")
-        return False
