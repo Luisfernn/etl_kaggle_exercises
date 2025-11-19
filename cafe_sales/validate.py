8987c8dc1ca3d2437aa6994bc7b1715206d7cc9e
Luis Fernando <luisfernandowubi@gmail.com>
2025-11-19 09:26:18 -0300

    Loop para validação de colunas específicas.

--- cafe_sales/validate.py
+++ cafe_sales/validate.py
@@ -7,3 +7,9 @@ logger.addHandler(logging.NullHandler())
 def validate_data(df: pd.DataFrame) -> bool:
     
     logger.info("🔍 Iniciando validação inteligente dos dados...")
+
+    required_cols = ["expected_total", "diff", "suspect_transaction"]
+    for col in required_cols:
+        if col not in df.columns:
+            logger.warning(f"⚠️ Coluna ausente: {col}")
+            return False
