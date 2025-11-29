69f1d7e12698705f59e6399746dabbd77a9714e1
Luis Fernando <luisfernandowubi@gmail.com>
2025-11-29 07:45:20 -0300

    Criando o scipt de main.

---
+++ restaurant_sales/main.py
@@ -0,0 +1,25 @@
+import logging
+from pathlib import Path
+from cafe_sales.pipeline import run_pipeline
+from cafe_sales.load import load_data
+
+BASE_DIR = Path(__file__).resolve().parent
+OUTPUT_DIR = BASE_DIR / "data" / "output"
+
+LOG_DIR = BASE_DIR / "data" / "logs"
+LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_DIR / "etl.log", mode="a", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+
+if __name__ == "__main__":
+    df = run_pipeline()
+
+    if df is not None:
+        load_data(df, OUTPUT_DIR)
