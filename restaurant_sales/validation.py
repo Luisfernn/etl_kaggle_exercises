e10a3843327ed67e1ba3889b6ec15f9d66d09f80
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-04 15:37:31 -0300

    Tirando a condição boleana da função e corrigindo indentação.

--- restaurant_sales/validation.py
+++ restaurant_sales/validation.py
@@ -2,10 +2,10 @@ from extract import extract_data
 import pandas as pd
 import logging
 
-def validation_data(df: pd.DataFrame) -> bool:
+def validation_data(df: pd.DataFrame):
 
     df = extract_data()
 
     df["valid_line"] = df["order_total"] > 0.01
 
- return df
+    return df
