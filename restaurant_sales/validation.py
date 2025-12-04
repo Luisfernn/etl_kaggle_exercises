6c75f3c57a41e6cb5ab25a8ddb8b52b250704cfc
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-04 15:34:31 -0300

    Criando função para definir quais linhas são válidas.

--- restaurant_sales/validation.py
+++ restaurant_sales/validation.py
@@ -1,3 +1,11 @@
 from extract import extract_data
 import pandas as pd
 import logging
+
+def validation_data(df: pd.DataFrame) -> bool:
+
+    df = extract_data()
+
+    df["valid_line"] = df["order_total"] > 0.01
+
+ return df
