a90b9d53c06a011c38a1cada82707dd947e4bd8d
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-05 10:36:31 -0300

    Definindo a variável valid (linhas válidas) do df.

--- restaurant_sales/validation_pre.py
+++ restaurant_sales/validation_pre.py
@@ -4,6 +4,8 @@ import logging
 
 def validation_data(df: pd.DataFrame) -> pd.DataFrame:
 
+    valid = df["valid_line"] == True
+    
     df["valid_line"] = df["order_total"] > 0.01
 
     return df
