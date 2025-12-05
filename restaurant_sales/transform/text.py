06e8b2803583e1f3f7ba4aa233194ca8bd3f8201
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-05 10:31:39 -0300

    Removendo a variável valid porque ela será realocada para validation_pre e não tem necessidade de ser declarada em outros módulos.

--- restaurant_sales/transform/text.py
+++ restaurant_sales/transform/text.py
@@ -2,7 +2,6 @@ import pandas as pd
 import logging
 
 def text_transformation(df, valid):
-    valid = df["valid_line"] == True
 
     df.loc[valid, "item"] = df.loc[valid, "item"].fillna("unknown")
 
