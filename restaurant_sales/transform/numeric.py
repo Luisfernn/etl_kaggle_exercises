e66c3c5975b502961bb0c4914d28e765523cdfad
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-05 10:37:33 -0300

    Adicionando espaços para organizar o código.

--- restaurant_sales/transform/numeric.py
+++ restaurant_sales/transform/numeric.py
@@ -2,6 +2,7 @@ import pandas as pd
 import logging
 
 def numeric_transformation(df, valid):
+    
     df.loc[valid, "price"] = df.loc[valid, "price"].fillna(0)
 
     df.loc[valid, "quantity"] = df.loc[valid, "quantity"].fillna(0).astype(int)
