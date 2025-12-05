085eb1420780318c7b1261f48806d1f04b9a498f
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-05 06:42:18 -0300

    Looping para preencher valores vazios de item com "unknown".

--- restaurant_sales/transform/text.py
+++ restaurant_sales/transform/text.py
@@ -3,3 +3,7 @@ import logging
 
 def text_transformation(df):
     df_valid = df["valid_line"] == True
+
+    for col in df:
+        if col == df_valid:
+            df["item"] = df["item"].fillna("unknown")
