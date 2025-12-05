55b717c3d7a89f9b84c4461faa704771a19b7f47
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-05 10:37:20 -0300

    Adicionando espaços para organizar o código.

--- restaurant_sales/validation_pre.py
+++ restaurant_sales/validation_pre.py
@@ -3,9 +3,9 @@ import pandas as pd
 import logging
 
 def validation_data(df: pd.DataFrame) -> pd.DataFrame:
-
-    valid = df["valid_line"] == True
     
+    valid = df["valid_line"] == True
+
     df["valid_line"] = df["order_total"] > 0.01
 
     return df
