55497ddc23aec7c8d8253eb0f6c88c33fbc4ed7f
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-07 08:09:28 -0300

    Validação para garantir que os valores de quantity foram transformados em int.

--- restaurant_sales/validation_post.py
+++ restaurant_sales/validation_post.py
@@ -5,3 +5,4 @@ import logging
 def validation_post(df):
 
     assert df["price"].dtype in ["float64", "int64"], "price is not numeric after transformation"
+    assert df["quantity"]dtype in ["int64"], "quantity not converted to integer"
