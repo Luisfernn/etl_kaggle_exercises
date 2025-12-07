828d12a18b3c62c70ce11c2138a2c28d14f96293
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-07 07:54:47 -0300

    Validação para garantir que todas as colunas obrigatórias estão no df.

--- restaurant_sales/validation_pre.py
+++ restaurant_sales/validation_pre.py
@@ -4,6 +4,10 @@ import logging
 
 def validation_data(df: pd.DataFrame) -> pd.DataFrame:
 
+    required_columns = {"item", "quantity", "payment_method", "valid_line"}
+    missing = required_columns - set(df.collumns)
+    assert len(missing) == =, f"Missing required columns: {missing}"
+
     valid = df["valid_line"] == True
 
     df["valid_line"] = df["order_total"] > 0.01
