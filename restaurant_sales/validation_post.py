7e76ada8019d33191683c0e854721dad50900113
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-07 08:31:43 -0300

    Contagem de NaN após as transformações.

--- restaurant_sales/validation_post.py
+++ restaurant_sales/validation_post.py
@@ -8,3 +8,8 @@ def validation_post_data(df):
     assert df["quantity"]dtype in ["int64"], "quantity not converted to integer"
     assert df["item"].notna().all(), "item has NaN after text cleaning"
     assert df["payment_method"].notna().all(), "payment_method has NaN after text cleaning"
+
+    nan_counts = df.isna().sum
+
+    logging.info("Pós_validation: Contagem de NaN por colunas:")
+    logging.info(f"\n{nan_counts}")
