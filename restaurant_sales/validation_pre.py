a1dbdc93f25abdfec508c8edf934eee171deb4d5
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-07 09:47:05 -0300

    Adicionando espaços para organizar o código separando cada transformação por bloco.

--- restaurant_sales/validation_pre.py
+++ restaurant_sales/validation_pre.py
@@ -8,14 +8,18 @@ def validation_pre_data(df: pd.DataFrame) -> pd.DataFrame:
     missing = required_columns - set(df.collumns)
     assert len(missing) == =, f"Missing required columns: {missing}"
 
+
     assert not df.empty, "DataFrame is empty after extract"
+
 
     if df.duplicated().any():
     logging.warning("There's duplicate lines in df.")
 
+
     valid = df["valid_line"] == True
 
     df["valid_line"] = df["order_total"] > 0.01
+    
 
     nan_counts = df.isna().sum
 
