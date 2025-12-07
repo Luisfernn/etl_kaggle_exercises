576db322a92d728e4eda1b8b352021a632a8d538
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-07 08:13:22 -0300

    Validação para garantir que payment_method não tenha NaN após a transformação.

--- restaurant_sales/validation_post.py
+++ restaurant_sales/validation_post.py
@@ -7,4 +7,4 @@ def validation_post(df):
     assert df["price"].dtype in ["float64", "int64"], "price is not numeric after transformation"
     assert df["quantity"]dtype in ["int64"], "quantity not converted to integer"
     assert df["item"].notna().all(), "item has NaN after text cleaning"
-    
+    assert df["payment_method"].notna().all(), "payment_method has NaN after text cleaning"
