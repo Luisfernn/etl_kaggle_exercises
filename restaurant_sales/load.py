6c501ad5cb41f0e074fb2b244792915c68d24007
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-08 15:52:13 -0300

    Salvando o arquivo.

--- restaurant_sales/load.py
+++ restaurant_sales/load.py
@@ -17,3 +17,13 @@ def load_data(df: pd.DataFrame, file_name: str = "restautant_sales_clean.csv", o
     if file_path.exists() and not overwrite:
         logger.error(f"❌ Arquivo já existe e overwrite=False: {file_path}")
         return None
+
+
+    try:
+        df.to_csv(file_path, index=False)
+        logger.info(f"📁 Arquivo salvo com sucesso: {file_path}")
+    except Exception as e:
+        logger.error(f"⚠️ Erro ao salvar arquivo: {e}")
+        return None
+
+    return file_path
