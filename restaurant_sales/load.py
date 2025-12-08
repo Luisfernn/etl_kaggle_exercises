d6ab9723e4a2861c8da6f4d47fd7b2ef6b79e22a
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-08 15:44:21 -0300

    Criando a função de salvamento.

--- restaurant_sales/load.py
+++ restaurant_sales/load.py
@@ -5,3 +5,5 @@ import logging
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 logger.propagate = False
+
+def load_data(df: pd.DataFrame, file_name: str = "restautant_sales_clean.csv", overwrite: bool = True):
