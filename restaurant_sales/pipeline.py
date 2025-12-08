befea755dabe6bbf3a2f1ded37c209a8ce0891ec
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-08 17:35:15 -0300

    Importando todos os módulos e bibliotecas.

---
+++ restaurant_sales/pipeline.py
@@ -0,0 +1,8 @@
+from extract import extract_data
+from validation_pre import validation_pre_data
+from transformation.text import text_transformation
+from transformation.numeric import numeric_transformation
+from validation_post import validation_post_data
+
+import pandas as pd
+import logging
