765f35ae44055b217f71b15f3b44c867eb87c3d6
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-08 16:33:36 -0300

    Definindo a variável etapa como None para poder usar dentro de try.

--- restaurant_sales/transform/numeric.py
+++ restaurant_sales/transform/numeric.py
@@ -3,6 +3,8 @@ import logging
 
 def numeric_transformation(df, valid):
     
+    etapa = None
+
     df.loc[valid, "price"] = df.loc[valid, "price"].fillna(0)
 
     df.loc[valid, "quantity"] = df.loc[valid, "quantity"].fillna(0).astype(int)
