e28076ccd3754df7a944834b22c6adad2ceb3cba
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-08 16:40:54 -0300

    Aplicando o conceito de try e execpt nas transformações numéricas para garantir que possíveis erros sejam evidenciados.

--- restaurant_sales/transform/numeric.py
+++ restaurant_sales/transform/numeric.py
@@ -4,11 +4,12 @@ import logging
 def numeric_transformation(df, valid):
     
     etapa = None
+    
+    try:
+        etapa = "Preenchendo com 0 linhas validas vazias em price"
+        df.loc[valid, "price"] = df.loc[valid, "price"].fillna(0)
 
-    etapa = "Preenchendo com 0 linhas validas vazias em price"
-    df.loc[valid, "price"] = df.loc[valid, "price"].fillna(0)
-
-    etapa = "Preenchendo com 0 linhas validas vazias em price e transformando os dados da coluna em int"
-    df.loc[valid, "quantity"] = df.loc[valid, "quantity"].fillna(0).astype(int)
+        etapa = "Preenchendo com 0 linhas validas vazias em price e transformando os dados da coluna em int"
+        df.loc[valid, "quantity"] = df.loc[valid, "quantity"].fillna(0).astype(int)
 
     return df
