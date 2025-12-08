3199623dc1c0874bb5b18eba7e20f1874f06f2a0
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-08 15:46:41 -0300

    Definindo os diretórios.

--- restaurant_sales/load.py
+++ restaurant_sales/load.py
@@ -7,3 +7,8 @@ logger.addHandler(logging.NullHandler())
 logger.propagate = False
 
 def load_data(df: pd.DataFrame, file_name: str = "restautant_sales_clean.csv", overwrite: bool = True):
+
+     output_dir = Path(__file__).resolve().parent / "data" / "output"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    file_path = output_dir / file_name
