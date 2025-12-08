08b7780658bf549ec34ee8781cab7b10af0d094a
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-08 16:37:07 -0300

    Usando a variável etapa para identificar cada operação.

--- restaurant_sales/transform/numeric.py
+++ restaurant_sales/transform/numeric.py
@@ -5,8 +5,10 @@ def numeric_transformation(df, valid):
     
     etapa = None
 
+    etapa = "Preenchendo com 0 linhas validas vazias em price"
     df.loc[valid, "price"] = df.loc[valid, "price"].fillna(0)
 
+    etapa = "Preenchendo com 0 linhas validas vazias em price e transformando os dados da coluna em int"
     df.loc[valid, "quantity"] = df.loc[valid, "quantity"].fillna(0).astype(int)
 
     return df
