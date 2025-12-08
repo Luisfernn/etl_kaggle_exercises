7c77fa47d37549a4feb0d0a7899b52da2b71edb4
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-08 17:36:23 -0300

    Importando o módulo de load.

--- restaurant_sales/pipeline.py
+++ restaurant_sales/pipeline.py
@@ -3,6 +3,7 @@ from validation_pre import validation_pre_data
 from transformation.text import text_transformation
 from transformation.numeric import numeric_transformation
 from validation_post import validation_post_data
+from load import load_data
 
 import pandas as pd
 import logging
