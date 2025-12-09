f75b693d70d9098210bf5b2e7b4f191fb2d505ff
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-09 12:29:28 -0300

    Definindo a variável como none para poder identificar cada transformação.

--- restaurant_sales/transform/text.py
+++ restaurant_sales/transform/text.py
@@ -7,9 +7,10 @@ logger.addHandler(logging.NullHandler())
 def text_transformation(df, valid):
 
     try:
-    
-    df.loc[valid, "item"] = df.loc[valid, "item"].fillna("unknown"), "❌ Erro ao preencher a coluna item"
+        etapa = None
+     
+       df.loc[valid, "item"] = df.loc[valid, "item"].fillna("unknown"), "❌ Erro ao preencher a coluna item"
 
-     df.loc[valid, "payment_method"] = df.loc[valid, "payment_method"].fillna("unknown"), "❌ Erro ao preencher a coluna payment_method"
+       df.loc[valid, "payment_method"] = df.loc[valid, "payment_method"].fillna("unknown"), "❌ Erro ao preencher a coluna payment_method"
 
-    return df
+     return df
