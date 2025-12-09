56106bab3f4963b4545ed6e45485aed7e88b700c
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-09 12:29:55 -0300

    Corrigindo indentação.

--- restaurant_sales/transform/text.py
+++ restaurant_sales/transform/text.py
@@ -8,9 +8,9 @@ def text_transformation(df, valid):
 
     try:
         etapa = None
-     
-       df.loc[valid, "item"] = df.loc[valid, "item"].fillna("unknown"), "❌ Erro ao preencher a coluna item"
 
-       df.loc[valid, "payment_method"] = df.loc[valid, "payment_method"].fillna("unknown"), "❌ Erro ao preencher a coluna payment_method"
+        df.loc[valid, "item"] = df.loc[valid, "item"].fillna("unknown"), "❌ Erro ao preencher a coluna item"
+
+        df.loc[valid, "payment_method"] = df.loc[valid, "payment_method"].fillna("unknown"), "❌ Erro ao preencher a coluna payment_method"
 
      return df
