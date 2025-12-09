28f37c67f701d510828de4b204d0155432dc83e0
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-09 08:24:43 -0300

    Excluindo o try pois como cada transformação possuí assert não tem necessidade de usar try e except.

--- restaurant_sales/validation_post.py
+++ restaurant_sales/validation_post.py
@@ -6,8 +6,6 @@ logger.addHandler(logging.NullHandler())
 
 def validation_post_data(df):
 
-    try:
-
     assert df["price"].dtype in ["float64", "int64"], "price is not numeric after transformation"
     assert df["quantity"]dtype in ["int64"], "quantity not converted to integer"
     assert df["item"].notna().all(), "item has NaN after text cleaning"
