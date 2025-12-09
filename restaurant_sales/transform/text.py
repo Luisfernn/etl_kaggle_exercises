a3304e3a2450d4b356b1b0e2f3fdc089f9fc2097
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-09 12:31:49 -0300

    Identificando uma transformação.

--- restaurant_sales/transform/text.py
+++ restaurant_sales/transform/text.py
@@ -8,7 +8,8 @@ def text_transformation(df, valid):
 
     try:
         etapa = None
-
+        
+        etapa = "Preencher com O linhas válidas vázias da coluna item"
         df.loc[valid, "item"] = df.loc[valid, "item"].fillna("unknown"), "❌ Erro ao preencher a coluna item"
 
         df.loc[valid, "payment_method"] = df.loc[valid, "payment_method"].fillna("unknown"), "❌ Erro ao preencher a coluna payment_method"
