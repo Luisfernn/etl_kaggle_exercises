c16affea4a8815f38b7d456637b55d2476e46715
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-09 08:45:34 -0300

    Corrigindo erro de escrita.

--- restaurant_sales/transform/text.py
+++ restaurant_sales/transform/text.py
@@ -8,6 +8,6 @@ def text_transformation(df, valid):
 
     assert df.loc[valid, "item"] = df.loc[valid, "item"].fillna("unknown"), "❌ Erro ao preencher a coluna item"
 
-    assert df.loc[valid, "payment_method"] = df.loc[valid, "payment_method"].fillna("unknown"), "❌ Erro ao pree preencher a coluna payment_method"
+    assert df.loc[valid, "payment_method"] = df.loc[valid, "payment_method"].fillna("unknown"), "❌ Erro ao preencher a coluna payment_method"
 
     return df
