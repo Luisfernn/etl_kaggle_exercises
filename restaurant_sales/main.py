feae2212141ca489afe056e0489a875a85e65507
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-10 14:24:36 -0300

    Corrigindo as importações.

--- restaurant_sales/main.py
+++ restaurant_sales/main.py
@@ -1,7 +1,7 @@
 import logging
 from pathlib import Path
-from cafe_sales.pipeline import run_pipeline
-from cafe_sales.load import load_data
+from pipeline import run_pipeline
+from load import load_data
 
 BASE_DIR = Path(__file__).resolve().parent
 OUTPUT_DIR = BASE_DIR / "data" / "output"
