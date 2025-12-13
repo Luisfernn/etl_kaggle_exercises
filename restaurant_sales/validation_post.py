19053185bbc3123e24b3ac0bf266540875f4eac2
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-13 10:39:25 -0300

    Corrigindo erro de síntaxe de price.

--- restaurant_sales/validation_post.py
+++ restaurant_sales/validation_post.py
@@ -6,7 +6,7 @@ logger.addHandler(logging.NullHandler())
 
 def validation_post_data(df):
 
-    assert df["price"].dtype in ["float64", "int64"], "price is not numeric after transformation"
+    assert is_numeric_dtype(df["price"]), "price is not numeric after transformation"
     assert df["quantity"].dtype in ["int64"], "quantity not converted to integer"
     assert df["item"].notna().all(), "item has NaN after text cleaning"
     assert df["payment_method"].notna().all(), "payment_method has NaN after text cleaning"
