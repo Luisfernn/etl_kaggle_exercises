66d5a2a19b9f1fb1252af5c8fd245e9b8cfa38c5
Luis Fernando <luisfernandowubi@gmail.com>
2025-12-21 11:35:26 -0300

    Tornando o aqruivo de extract executável.

--- iranian_transactions/extract.py
+++ iranian_transactions/extract.py
@@ -29,4 +29,6 @@ if __name__ == "__main__":
     handler = logging.StreamHandler()
     handler.setFormatter(logging.Formatter("%(message)s"))
     logger.addHandler(handler)
-    logger.setLevel(logging.INFO)            
+    logger.setLevel(logging.INFO)
+
+    df = extract_data()            
